706dc527db085292ebe86073ab2fc2a038f5d8a7
NguyenVu124 <hoangnguyenvubk@gmail.com>
2021-09-04 20:49:37 +0700

    Design trello Frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,164 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/81496118_1029471677407304_1627303837905715200_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=nmdhMOjO5VUAX-kODsH&_nc_ht=scontent-hkg4-1.xx&oh=92735a59273dcc3a001821ce45c68377&oe=615816E9"
+                alt="photo"
+              />
+              Title: NguyenVu
+            </li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+            <li>Add what you like</li>
+          </ul>
+          <footer>Add another</footer>
+        </div>
+      </div>
     </div>
   );
 }
